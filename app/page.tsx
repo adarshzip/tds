f9d4b0a0e66a0b7dc8feb3def4f24ca7d1c150d7
Adarsh Agrawal <155928266+adarshzip@users.noreply.github.com>
2024-05-04 16:08:42 -0700

    fixed footer + homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,41 +17,30 @@ export default function Home() {
           <h2 className="text-white font-semibold text-3xl rounded-md sm:px-1 bg-usc-red" >Join the <b className="text-usc-yellow">Trojan Debate Squad</b>.</h2>
         </div>
       </div>
-      <div className="flex items-center">
-        <div className="bg-white py-5 px-6 flex flex-col lg:basis-3/4 gap-6 xl:pl-20 lg:py-10">
-          <div className="flex flex-col">
-            <h2 className="text-usc-red font-Caslon text-2xl">About the Team</h2>
-            <p className="text-black lg:w-2/3">The Trojan Debate Squad (TDS), USC&apos;s first chartered student organization, is devoted to teaching argumentation and analytic thinking skills to students through academic debate. The TDS is one of the nation&apos;s leading collegiate debate programs and focuses on competitive policy debate and expanding debate in Los Angeles public schools.</p>
-            <p className="text-black"><a href="/about" className="text-usc-red underline">Learn More</a>.</p>
-          </div>
-          <div className="flex flex-col basis-1/2">
-            <h2 className="text-usc-red font-Caslon text-2xl">Join Us</h2>
-            <p className="text-black lg:w-2/3">Whether you&apos;re an incoming student, current trojan, or even an interested high schooler, we&apos;re always excited to have new members on the team.</p>
-            <p className="text-black"><a href="/prospects" className="text-usc-red underline">Learn More</a>.</p>
-          </div>
-          <div className="flex flex-col basis-1/2">
-            <h2 className="text-usc-red font-Caslon text-2xl">Outreach</h2>
-            <p className="text-black lg:w-2/3"><a href="/outreach" className="text-usc-red underline">Learn more</a> about our efforts to expand access to debate throughout Los Angeles.</p>
-          </div>
-          <div className="flex flex-col basis-1/2">
-            <h2 className="text-usc-red font-Caslon text-2xl">Donate</h2>
-            <p className="text-black lg:w-2/3">We rely on the generous support of our donors and the USC Annenberg School of Communications to fund our competitive efforts.</p>
-            <p className="text-black"><a href="/donate" className="text-usc-red underline">Learn More</a>.</p>
-          </div>
-          <div className="flex flex-col basis-1/2">
-            <h2 className="text-usc-red font-Caslon text-2xl">Contact</h2>
-            <p className="text-black lg:w-2/3">Any questions not already covered? Feel free to <a href="/contact" className="text-usc-red underline">reach out</a>.</p>
-          </div>
+      <div className="bg-white py-5 px-6 flex flex-col gap-6 mx-auto max-w-6xl">
+        <div className="flex flex-col">
+          <h2 className="text-usc-red font-Caslon text-2xl">About the Team</h2>
+          <p className="text-black">The Trojan Debate Squad (TDS), USC&apos;s first chartered student organization, is devoted to teaching argumentation and analytic thinking skills to students through academic debate. The TDS is one of the nation&apos;s leading collegiate debate programs and focuses on competitive policy debate and expanding debate in Los Angeles public schools.</p>
+          <p className="text-black"><a href="/about" className="text-usc-red underline">Learn More</a>.</p>
         </div>
-        <Image
-          src={teamImg}
-          alt = "Image of a few members of the USC Debate Team standing in a parking lot, smiling."
-          style={{
-            maxHeight: '500px',
-            height: 'auto',
-            width: 'auto',
-          }}
-        />
+        <div className="flex flex-col">
+          <h2 className="text-usc-red font-Caslon text-2xl">Join Us</h2>
+          <p className="text-black">Whether you&apos;re an incoming student, current trojan, or even an interested high schooler, we&apos;re always excited to have new members on the team.</p>
+          <p className="text-black"><a href="/prospects" className="text-usc-red underline">Learn More</a>.</p>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-usc-red font-Caslon text-2xl">Outreach</h2>
+          <p className="text-black"><a href="/outreach" className="text-usc-red underline">Learn more</a> about our efforts to expand access to debate throughout Los Angeles.</p>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-usc-red font-Caslon text-2xl">Donate</h2>
+          <p className="text-black">We rely on the generous support of our donors and the USC Annenberg School of Communications to fund our competitive efforts.</p>
+          <p className="text-black"><a href="/donate" className="text-usc-red underline">Learn More</a>.</p>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-usc-red font-Caslon text-2xl">Contact</h2>
+          <p className="text-black">Any questions not already covered? Feel free to <a href="/contact" className="text-usc-red underline">reach out</a>.</p>
+        </div>
       </div>
     </main>
   );
--- components/elements/Footer.tsx
+++ components/elements/Footer.tsx
@@ -1,15 +1,23 @@
 export default function Header() {
     return(
-        <footer className="bg-usc-red flex items-center pl-4 font-light font-Source_Sans_3 basis-full justify-between">
-            <div className='flex flex-row font-Caslon py-5'>
-                <a className='flex pr-0 text-3xl text-usc-yellow' href='/'>
-                    USC
-                </a>
-                <a className="flex pr-6 text-3xl text-white" href='/'>
-                Debate
-                </a>
+        <footer className="bg-usc-red">
+            <div className="flex items-center pl-4 font-light font-Source_Sans_3 basis-full justify-between">
+                <div className='flex flex-row font-Caslon py-5'>
+                    <a className='flex pr-0 text-3xl text-usc-yellow' href='/'>
+                        USC
+                    </a>
+                    <a className="flex pr-6 text-3xl text-white" href='/'>
+                    Debate
+                    </a>
+                </div>
+                <div className="flex flex-row">
+                    <a href="/sitemap" className="pr-6 hover:text-usc-yellow text-lg">Sitemap</a>
+                </div>
             </div>
-            <a href="/sitemap" className="pr-6 hover:text-usc-yellow text-lg">Sitemap</a>
+            <div className="flex items-center px-4 pb-4 font-light font-Source_Sans_3 basis-full justify-between">
+                <p className="font-thin font-sans text-xs">Copyright © 2024 University of Southern California Trojan Debate Squad</p>
+                <p className="font-thin font-sans text-xs">Designed and built by <a href="https://adarsh.zip" className="text-white hover:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] hover:from-green-300 hover:via-emerald-400 hover:to-cyan-400 hover:text-transparent hover:bg-clip-text">Adarsh Agrawal</a>.</p>
+            </div>
         </footer>
     )
 }
