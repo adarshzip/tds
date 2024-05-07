ee022fecddd579c3a77b87f5af6666b85fe47c04
Adarsh Agrawal <155928266+adarshzip@users.noreply.github.com>
2024-05-06 19:22:39 -0700

    fixed issues for non-edge web browsers

--- components/elements/Footer.tsx
+++ components/elements/Footer.tsx
@@ -13,12 +13,12 @@ export default function Header() {
                     </Link>
                 </div>
                 <div className="flex flex-row">
-                    <Link href="/sitemap" className="pr-6 transition duration-150 ease-in hover:text-usc-yellow text-lg font-sans">Sitemap</Link>
+                    <Link href="/sitemap" className="pr-6 transition duration-150 ease-in text-white hover:text-usc-yellow text-lg font-sans">Sitemap</Link>
                 </div>
             </div>
             <div className="flex items-center px-4 pb-4 font-light font-source_sans basis-full justify-between">
-                <p className="font-thin font-source_sans text-xs">Copyright © 2024 USC Trojan Debate Squad</p>
-                <p className="hidden md:flex font-thin font-sans text-xs">Designed and built by&nbsp;<Link href="https://adarsh.zip" className="text-white hover:font-medium hover:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] hover:from-green-300 hover:via-emerald-400 hover:to-cyan-400 hover:text-transparent hover:bg-clip-text">Adarsh Agrawal</Link>.</p>
+                <p className="font-thin font-source_sans text-xs text-white">Copyright © 2024 USC Trojan Debate Squad</p>
+                <p className="hidden md:flex font-thin font-sans text-xs text-white">Designed and built by&nbsp;<Link href="https://adarsh.zip" className="text-white hover:font-medium hover:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] hover:from-green-300 hover:via-emerald-400 hover:to-cyan-400 hover:text-transparent hover:bg-clip-text">Adarsh Agrawal</Link>.</p>
             </div>
         </footer>
     )
